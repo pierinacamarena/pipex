af78fc9799b6914f8f1fa4c39ef6c366bf45b49f
Pierina <pierinaacam@gmail.com>
2021-10-26 18:07:27 +0200

    Norminette advance

--- pipex.c
+++ pipex.c
@@ -12,92 +12,93 @@
 
 #include "pipex.h"
 
-char    *ft_path(char *arg, char **env)
+char	*ft_path(char *arg, char **env)
 {
-    int     finder;
-    int     i;
-    int     j;
-    char    *path;
-    char    *cmd;
-    char    **path_split;
+	int		finder;
+	int		i;
+	int		j;
+	char	*path;
+	char	*cmd;
+	char	**path_split;
 
-    i = 0;
-    while (env[i])
-    {
-        finder = ft_strncmp(env[i], "PATH=", 5);
-        if (finder == 0)
-            path = env[i];
-        i++;
-    }
-    path = path + 5;
-    path_split = ft_strsplit(path, ':');
-    i = 0;
-    while (path_split[i])
-    {
-        j = ft_strlen(path_split[i]) - 1;
+	i = 0;
+	while (env[i])
+	{
+		finder = ft_strncmp(env[i], "PATH=", 5);
+		if (finder == 0)
+			path = env[i];
+		i++;
+	}
+	path = path + 5;
+	path_split = ft_strsplit(path, ':');
+	i = 0;
+	while (path_split[i])
+	{
+		j = ft_strlen(path_split[i]) - 1;
 		if (path_split[i][j] == '/')
-		    cmd = ft_strjoin(path_split[i], arg);
+			cmd = ft_strjoin(path_split[i], arg);
 		else
-		    cmd = ft_str3join(path_split[i], "/", arg);
+			cmd = ft_str3join(path_split[i], "/", arg);
 		if (access(cmd, F_OK | X_OK) == 0)
 		{
-		    ft_free(path_split);
-		    return (cmd);
+			ft_free(path_split);
+			return (cmd);
 		}
 		free(cmd);
 		i++;
-    }
-    ft_free(path_split);
-    return(NULL);
+	}
+	ft_free(path_split);
+	return (NULL);
 }
 
-void    exec(char *cmd, char **env)
-{
-    char    **command;
-    char    *path;
 
-    command = ft_strsplit(cmd, ' ');
+
+void	exec(char *cmd, char **env)
+{
+	char	**command;
+	char	*path;
+
+	command = ft_strsplit(cmd, ' ');
 	path = ft_path(command[0], env);
 	if (execve(path, command, env) == -1)
 	{
-	    printf("command not found \n");
-	    free(path);
-	    ft_free(command);
-	    exit(1);
+		printf("command not found \n");
+		free(path);
+		ft_free(command);
+		exit(1);
 	}
 }
 
-void    pipex(char **av, char **env, int fdin)
+void	pipex(char **av, char **env, int fdin)
 {
-    int     end[2];
-    pid_t   child;
+	int	end[2];
+	pid_t	child;
 
-    pipe(end);
-    child = fork();
-    if (child == 0)
-    {
-        close(end[0]);
-        dup2(end[1], STDOUT);
-    	if (fdin == STDIN)
+	pipe(end);
+	child = fork();
+	if (child == 0)
+	{
+		close(end[0]);
+		dup2(end[1], STDOUT);
+		if (fdin == STDIN)
 			exit(1);
-        exec(av[2], env);
-        close(end[1]);
-    }
-    else
-    {
-        waitpid(child, NULL, 0);
-        close(end[1]);
+		exec(av[2], env);
+		close(end[1]);
+	}
+	else
+	{
+		waitpid(child, NULL, 0);
+		close(end[1]);
 		dup2(end[0], STDIN);
 		exec(av[3], env);
 		close(end[0]);
-    }
+	}
 }
 
-
-int main (int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
-	int		fdin;
-	int		fdout;
+	int	fdin;
+	int	fdout;
 
 	if (ac == 5)
 	{
@@ -108,13 +109,13 @@ int main (int ac, char **av, char **env)
 		pipex(av, env, fdin);
 		close(fdin);
 		close(fdout);
-    }
-    else
+	}
+	else
 		write(STDERR, "Invalid number of arguments.\n", 29);
 	return (1);
 }
 
-int	openfile (char *filename, int mode)
+int	openfile(char *filename, int mode)
 {
 	if (mode == INFILE)
 	{
